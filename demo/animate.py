a64aa05a72492696356593f8737149b3dcf23498
Nancho-lyh <3474311240@qq.com>
2024-01-11 01:00:28 +0800

    change background by SAM pre_trained model

--- demo/animate.py
+++ demo/animate.py
@@ -9,7 +9,6 @@
 # without an express license agreement from ByteDance or
 # its affiliates is strictly prohibited.
 import argparse
-import argparse
 import datetime
 import inspect
 import os
@@ -17,30 +16,76 @@ import numpy as np
 from PIL import Image
 from omegaconf import OmegaConf
 from collections import OrderedDict
-
 import torch
 
-from diffusers import AutoencoderKL, DDIMScheduler, UniPCMultistepScheduler
-
+from diffusers import AutoencoderKL, DDIMScheduler, UniPCMultistepScheduler,StableDiffusionInpaintPipeline
 from tqdm import tqdm
 from transformers import CLIPTextModel, CLIPTokenizer
-
+from magicanimate.utils.crop_for_replacing import resize_and_pad,recover_size
 from magicanimate.models.unet_controlnet import UNet3DConditionModel
 from magicanimate.models.controlnet import ControlNetModel
 from magicanimate.models.appearance_encoder import AppearanceEncoderModel
 from magicanimate.models.mutual_self_attention import ReferenceAttentionControl
 from magicanimate.pipelines.pipeline_animation import AnimationPipeline
-from magicanimate.utils.util import save_videos_grid
+from magicanimate.utils.util import save_videos_grid,load_img_to_array,save_array_to_img
 from accelerate.utils import set_seed
-
+from segment_anything import SamPredictor, sam_model_registry
 from magicanimate.utils.videoreader import VideoReader
-
+from typing import Any, Dict, List
 from einops import rearrange, repeat
 
 import csv, pdb, glob
 from safetensors import safe_open
 import math
 from pathlib import Path
+
+
+def replace_img_with_sd(
+        img: np.ndarray,
+        mask: np.ndarray,
+        text_prompt: str,
+        step: int = 50,
+        device="cuda"
+):
+    pipe = StableDiffusionInpaintPipeline.from_pretrained(
+        "pretrained_models/stable-diffusion-2-inpainting",
+        torch_dtype=torch.float32,
+    ).to(device)
+    img_padded, mask_padded, padding_factors = resize_and_pad(img, mask)
+    img_padded = pipe(
+        prompt=text_prompt,
+        image=Image.fromarray(img_padded),
+        mask_image=Image.fromarray(255 - mask_padded),
+        num_inference_steps=step,
+    ).images[0]
+    height, width, _ = img.shape
+    img_resized, mask_resized = recover_size(
+        np.array(img_padded), mask_padded, (height, width), padding_factors)
+    mask_resized = np.expand_dims(mask_resized, -1) / 255
+    img_resized = img_resized * (1-mask_resized) + img * mask_resized
+    return img_resized
+
+def predict_masks_with_sam(
+        img: np.ndarray,
+        point_coords: List[List[float]],
+        point_labels: List[int],
+        model_type: str,
+        ckpt_p: str,
+        device="cuda"
+):
+    point_coords = np.array(point_coords)
+    point_labels = np.array(point_labels)
+    sam = sam_model_registry[model_type](checkpoint=ckpt_p)
+    sam.to(device=device)
+    predictor = SamPredictor(sam)
+
+    predictor.set_image(img)
+    masks, scores, logits = predictor.predict(
+        point_coords=point_coords,
+        point_labels=point_labels,
+        multimask_output=True,
+    )
+    return masks, scores, logits
 
 class MagicAnimate():
     def __init__(self, config="configs/prompts/animation.yaml") -> None:
@@ -126,6 +171,15 @@ class MagicAnimate():
         print("Initialization Done!")
         
     def __call__(self, source_image, motion_sequence, random_seed, step, guidance_scale, size=512):
+            config = OmegaConf.load("configs/prompts/animation.yaml")
+            parser = argparse.ArgumentParser()
+            parser.add_argument("--point_coords", type=float, nargs='+', required=True,
+        help="The coordinate of the point prompt, [coord_W coord_H].",)
+            parser.add_argument(
+                "--point_labels", type=int, nargs='+', required=True,
+                help="The labels of the point prompt, 1 or 0.",
+            )
+            args = parser.parse_args()
             prompt = n_prompt = ""
             random_seed = int(random_seed)
             step = int(step)
@@ -143,7 +197,60 @@ class MagicAnimate():
                 if control[0].shape[0] != size:
                     control = [np.array(Image.fromarray(c).resize((size, size))) for c in control]
                 control = np.array(control)
-            
+
+            latest_coords = args.point_coords
+              # img = load_img_to_array(config.input_img)
+            img = source_image
+            masks, _, _ = predict_masks_with_sam(
+                img,
+                [latest_coords],
+                args.point_labels,
+                model_type=config.sam_model_type,
+                ckpt_p=config.sam_ckpt,
+                device="cuda",
+            )
+            masks = masks.astype(np.uint8) * 255
+
+            # # dilate mask to avoid unmasked edge effect
+            # if args.dilate_kernel_size is not None:
+            #     masks = [dilate_mask(mask, args.dilate_kernel_size) for mask in masks]
+
+            # visualize the segmentation results
+            img_stem = "Test"
+            out_dir = Path(config.output_dir) / img_stem
+            out_dir.mkdir(parents=True, exist_ok=True)
+            for idx, mask in enumerate(masks):
+                # path to the results
+                mask_p = out_dir / f"mask_{idx}.png"
+                img_points_p = out_dir / f"with_points.png"
+                img_mask_p = out_dir / f"with_{Path(mask_p).name}"
+
+                # save the mask
+                save_array_to_img(mask, mask_p)
+
+                # save the pointed and masked image
+                # dpi = plt.rcParams['figure.dpi']
+                # height, width = img.shape[:2]
+                # plt.figure(figsize=(width / dpi / 0.77, height / dpi / 0.77))
+                # plt.imshow(img)
+                # plt.axis('off')
+                # show_points(plt.gca(), [latest_coords], args.point_labels,
+                #             size=(width * 0.04) ** 2)
+                # plt.savefig(img_points_p, bbox_inches='tight', pad_inches=0)
+                # show_mask(plt.gca(), mask, random_color=False)
+                # plt.savefig(img_mask_p, bbox_inches='tight', pad_inches=0)
+                # plt.close()
+
+            # fill the masked image
+            for idx, mask in enumerate(masks):
+                # if config.seed is not None:
+                #     torch.manual_seed(config.seed)
+                mask_p = out_dir / f"mask_{idx}.png"
+                img_replaced_p = out_dir / f"replaced_with_{Path(mask_p).name}"
+                source_image = replace_img_with_sd(
+                    img, mask, config.text_prompt, device="cuda")
+                save_array_to_img(source_image, img_replaced_p)
+
             if source_image.shape[0] != size:
                 source_image = np.array(Image.fromarray(source_image).resize((size, size)))
             H, W, C = source_image.shape
